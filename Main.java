2c8760e52c3f04c29a009def5e6682203fa9fac1
pahalagehdevindi820 <pahalagehdevindi820@gmail.com>
2025-05-20 10:13:16 +0530

    abc

--- Main.java
+++ Main.java
@@ -4,19 +4,19 @@ import java.awt.event.ActionListener;
 
 public class Main {
     public static void main(String[] args) {
-        // Create frame
+        
         JFrame frame = new JFrame("Restaurant Home Page");
         frame.setSize(400, 300);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-        // Create New User button
+    
         JButton newUserButton = new JButton("New User");
         newUserButton.setBounds(150, 100, 100, 40);
 
-        // Add action to button
+    
         newUserButton.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
-                new NewUserForm(); // Open the new user form
+                new NewUserForm();
             }
         });
 
